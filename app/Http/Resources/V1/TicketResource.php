c30cced95798e6b4201d96e417de76b096e217f1
sagar gautam <summersharma226@gmail.com>
2025-11-03 15:43:56 +0545

    Designing Response Payloads

---
+++ app/Http/Resources/V1/TicketResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResource extends JsonResource
+{
+    // public static $wrap = 'ticket';
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'ticket',
+            'id' => $this->id,
+            'attributes' => [
+                'title' => $this->title,
+                'description' => $this->description,
+                'status' => $this->status,
+            ],
+           
+            'author' => [
+                'data' => [
+                    'type' => 'user',
+                    'id' => $this->user_id,
+                ],
+                'links' => [
+                    'self' => 'todo',
+                ]
+            ],
+            
+             'links' => [
+                'self' => url('/api/v1/tickets/' . $this->id),
+            ],
+        ];
+    }
+}
