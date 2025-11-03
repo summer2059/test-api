ee2dd4c8efed9941d612fe14074ef09a08b3925e
sagar gautam <summersharma226@gmail.com>
2025-11-03 13:28:05 +0545

    designing the url

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Ticket;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+      $user =  \App\Models\User::factory(10)->create();
+
+      Ticket::factory(100)->recycle($user)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//http://localhost:8000/api ,,  http://localhost:8000/api/tickets,, http://localhost:8000/api/tickets/{id}/,, http://localhost:8000/api/tickets/{id}/edit
+
+//universal  resource locator
+//tickets
+//users
+//contracts
+
 // Route::get('/', function() {
 //     return response()->json([
 //         'message' => 'Hello World'
@@ -22,6 +30,11 @@ use Illuminate\Support\Facades\Route;
 // }); 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function() {
+    return Ticket::all();
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
