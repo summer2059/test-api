1f7870d3b4c3af07560f63910484f4e3d0aa3038
sagar gautam <summersharma226@gmail.com>
2025-11-04 15:52:40 +0545

    Using Policies for User Authorization

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -11,6 +11,7 @@ class ApiController extends Controller
 
     use ApiResponses;
 
+    protected $policyClass;
     
     public function include(string $relationship){
         $param = request()->get('include');
@@ -21,5 +22,9 @@ class ApiController extends Controller
         $includeValues = explode(',', strtolower($param));
 
         return in_array(strtolower($relationship), $includeValues);
+    }
+
+    public function isAble($ability, $targetModel) {
+        return $this->authorize($ability, [$targetModel, $this->policyClass]);
     }
 }
--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -9,10 +9,14 @@ use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\User;
+use App\Policies\V1\TicketPolicy;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
 
 class TicketController extends ApiController
 {
+    protected $policyClass = TicketPolicy::class;
     /**
      * Display a listing of the resource.
      */
@@ -83,10 +87,18 @@ class TicketController extends ApiController
     {
         try {
             $ticket = Ticket::findOrFail($ticket_id);
+
+            // $request->user()->tokenCan('ticket:update');
+            // $request->user()->id == $ticket->user_id;
+            //policy
+            $this->isAble('update', $ticket);
+
             $ticket->update( $request->mappedAttributes());
             return new TicketResource($ticket);
         } catch (ModelNotFoundException $expeption) {
             return $this->error('Ticket not found', 404);
+        } catch (AuthorizationException $ex) {
+            return $this->error('You are not authorized to update this ticket', 404);
         }
     }
 
